2739f23f2a7a123ece0603050bf45ac005b93403
Robert Smiles Jr <smilesr120@gmail.com>
2018-11-28 06:34:20 -0500

    added delete todo route

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.get('/todos', (req, res) => {
 });
 
 app.get('/todos/:id', (req, res) => {
-  var id = req.params.id;
+  let id = req.params.id;
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
@@ -48,6 +48,25 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
+
+app.delete('/todos/:id', (req, res) =>{
+  let id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
